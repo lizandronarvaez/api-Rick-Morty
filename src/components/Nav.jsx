187e296cc5c24f0cc20021329c099f70dafebca9
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2022-09-24 14:13:37 +0200

    Aplicando Responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
         <div className='nav'>
             <img src="https://i0.wp.com/clickwallpapers.net/wp-content/uploads/2022/06/clickwallpapers-rickandmorty-desenho-4k-img8-minbase-scaled-1.jpg?fit=2560%2C1440&ssl=1" alt='img'/>
             <h1>Rick and Morty</h1>
-            <h1>Buscador de Personajes</h1>
+            <h2>Buscador de Personajes</h2>
         </div>
     )
 }
